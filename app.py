8f296b98a994e397c2299f184f532d2a087a6f99
rezvanghd <rezvanghadamiari14@gmail.com>
2020-07-04 11:09:04 +0430

    add bootstrap template

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 import requests
 import config
 
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
+    app.run()
+
 city_name = input()
 API_KEY = config.API_KEY
 
